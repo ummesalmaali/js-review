47997694c9de4290fe48cd06b4a9dbbaedb7102c
ummesalmaali <ummes3527@gmail.com>
2021-09-02 20:28:06 +0600

    done

--- script.js
+++ script.js
@@ -10,19 +10,19 @@ const reviews = [
         id:2,
         name:"anna jhonson ",
         job:"web developer",
-        img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-np0x5l.jpg",
+        img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
         text:"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
     },
     {
         id:3,
-        name:"",
+        name:"Jhon Doe",
         job:"web developer",
         img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
         text:"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
     },
     {
         id:3,
-        name:"",
+        name:"Alan Walker",
         job:"web developer",
         img:"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
         text:"I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
@@ -31,7 +31,7 @@ const reviews = [
 
 // select the DOM elements
 const img = document.getElementById("prsn-img");
-const author = document.getElementById("author");
+const athor = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("job");
 
@@ -39,3 +39,41 @@ const info = document.getElementById("job");
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function(){
+showPerson(currentItem)
+})
+
+// show person based on item
+function showPerson(){
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener("click", function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+   
+    showPerson(currentItem);
+})
+prevBtn.addEventListener("click", function(){
+    currentItem--
+if(currentItem<0){
+    currentItem = reviews.length - 1;
+    if(currentItem < 0){
+        currentItem.reviews.length - 1
+    }
+}
+showPerson()
+})
